861ddafba61cd324f3f2d343b7ced92319738146
quattromani <max@maxquattromani.com>
2014-09-20 23:23:01 -0600

    added ranking function

--- js/build/production.js
+++ js/build/production.js
@@ -71,25 +71,44 @@ $.ajax({
   url: '/json/leaderboard.json',
   success: function(data) {
 
+    var rank = 0;
+    var rankGames = 0;
+    var counter = 1;
+
     // Get the number of players in the pool
     var playerCount = data.length;
     $('<span>'+playerCount+'</span>').appendTo('.player-count');
 
     // Set the end of season pool total
-    var seasonPot = "$105"
+    var seasonPot = "$105";
     $('<span>'+seasonPot+' USD</span>').appendTo('.season-pot');
 
     var tr;
     data.sort(function(a,b) { return parseFloat(b.total) - parseFloat(a.total) } );
 
-    for (var i = 0; i < data.length; i++) {
+    for (i = 0; i < data.length; i++) {
+
       var gamesWon = data[i].total;
 
+      if (rankGames != gamesWon) {
+        var rank = counter;
+        var rankGames = gamesWon;
+      } else if (rankGames == gamesWon) {
+        var rank = rank;
+      }
+
+      if (i == 0) {
+        var divisor = (100 / gamesWon);
+      }
+
       var tdWidth = ((gamesWon / gamesTotal) *100).toFixed(2) + "%";
+      var winPct = (gamesWon * divisor).toFixed(2) + "%";
 
       tr = $('<tr/>');
-      tr.append('<td style="width: ' + tdWidth +'"><span class="player left">' + data[i].player + '</span><span class="total right" data-total="' + data[i].total + '" data-pct="' + tdWidth + '"></span></td>');
+      tr.append('<td style="width: ' + winPct +'"><span class="rank left">' + rank + '</span><span class="player left">' + data[i].player + '</span><span class="total right" data-total="' + data[i].total + '" data-pct="' + tdWidth + '"></span></td>');
       $('table').append(tr);
+
+      counter++;
     }
   }
 });
