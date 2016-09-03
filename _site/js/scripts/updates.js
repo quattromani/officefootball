8db96c47521ce5a05c0412260aab51246a6e9623
quattromani <max@maxquattromani.com>
2016-09-03 09:25:17 -0600

    updates scores page js and duplicated logos for new feed names

--- _site/js/scripts/updates.js
+++ _site/js/scripts/updates.js
@@ -1,10 +1,10 @@
- $.getJSON('https://spreadsheets.google.com/feeds/list/1JCa8acwW8ATlbeaWugatJfM08aKehFcVQuShI8KRg7A/1/public/values?alt=json', function(data) {
+ $.getJSON('http://www.nfl.com/liveupdate/scorestrip/ss.json', function(data) {
 
-  for (i = 0; i < data.feed.entry.length; i++) {
-    var home = data.feed.entry[i]['gsx$hnn']['$t'];
-    var homeScore = data.feed.entry[i]['gsx$hs']['$t'];
-    var visitor = data.feed.entry[i]['gsx$vnn']['$t'];
-    var visitorScore = data.feed.entry[i]['gsx$vs']['$t'];
+  for (i = 0; i < data.gms.length; i++) {
+    var home = data.gms[i].h;
+    var homeScore = data.gms[i].hs;
+    var visitor = data.gms[i].v;
+    var visitorScore = data.gms[i].vs;
 
     var result = Math.max(homeScore,visitorScore);
     if(result >= 0) {
@@ -37,10 +37,8 @@
       ');
   }
 
-  var timestamp = new Date(data.feed.entry[0]['updated']['$t']);
+  var week = data.w;
 
-  $('.updated').append('<div class="text-center"><span>Updated: ' + timestamp + '</span></div>');
+  $('.scoreboard h1').append('<div class="text-center"><span>Week '+week+' Scores</span></div>');
 
 });
-
-
