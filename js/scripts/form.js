37ee8869ed76824b23b5ab9ba4b85834c63eba25
quattromani <max@maxquattromani.com>
2016-09-08 16:54:03 -0600

    added myself to cc

--- js/scripts/form.js
+++ js/scripts/form.js
@@ -7,8 +7,7 @@ $("#weekly").on("submit", function () {
 $(function() {
   $('#email').change(function() {
     var value = $(this).val();
-    $('#copy').val(value);
-
+    $('#copy').val('max@maxquattromani.com,' + value);
   });
 });
 
