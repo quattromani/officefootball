2e66e0aec6e07e6ea82e571733c53d41f34fc916
quattromani <max@maxquattromani.com>
2016-09-08 19:28:05 -0600

    specificity of disabled input

--- _site/js/scripts/picks.js
+++ _site/js/scripts/picks.js
@@ -168,7 +168,7 @@ function getData(week) {
         // if a game has started, disable it's selection and do not make it required
         var now = new Date();
         if (now > timestamp) {
-          $('input').removeAttr('required').attr('disabled', true);
+          $('input[type=radio]').removeAttr('required').attr('disabled', true);
           console.log(now);
           console.log(timestamp);
         }
