15d7e344a934efb04f2021db61f51563c1e817c4
quattromani <max@maxquattromani.com>
2016-12-11 12:02:49 -0700

    remove 'view picks' until all picks are in

---
+++ js/scripts/nav.js
@@ -0,0 +1,10 @@
+$(function() {
+  today = new Date()
+  dayIndex = today.getDay()
+  console.log(dayIndex);
+
+  if(dayIndex == '2' || dayIndex == '3' || dayIndex == '4') {
+    $('.view-picks').css('display', 'none');
+  }
+
+});
