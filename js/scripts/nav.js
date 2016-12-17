7e4080d4c6e5a55798e0a06f1bfe0fcfe6f98d27
quattromani <max@maxquattromani.com>
2016-12-17 08:49:24 -0700

    fixed erroneous date causing disabling for prior games

--- js/scripts/nav.js
+++
@@ -1,9 +0,0 @@
-// $(function() {
-//   today = new Date()
-//   dayIndex = today.getDay()
-//   console.log(dayIndex);
-
-//   if(dayIndex == '2' || dayIndex == '3' || dayIndex == '4') {
-//     $('.view-picks').css('display', 'none');
-//   }
-// });
